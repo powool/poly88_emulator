e7efa086c8e6f07b6f2e6412e68469053665002f
Powool <powool@gmail.com>
2026-04-14 15:59:38 -0400

    finish implementing and testing tiered memory

--- TieredMemory.cpp
+++ TieredMemory.cpp
@@ -1,63 +1,23 @@
+#include "IntelHex.hpp"
 #include "TieredMemory.hpp"
 
-TieredMemory::Storage::Storage(std::string filename)
+// load an image in Intel Hex format
+Storage::Storage(const std::string &filename, bool readWrite) :
+	readWrite(readWrite)
 {
-    FILE *stream;
-    int addr;
-    char buf[1024];
-    int new_addr,v2,v3, v1;
-    int item_cnt;
+	// this can throw IntelHexException
+	IntelHex hex(filename);
+	data = hex.Data();
+	address = hex.Address();
+}
 
-    stream = fopen(filename.c_str(),"r");
-    if(stream==NULL)
-    {
-		std::error_code ec(errno, std::generic_category());
-		throw std::system_error(ec, std::string("failed to open ROM file ") + filename);
-    }
-    addr = 0;
-    while(fgets(buf, sizeof(buf), stream))
-    {
-        if(isxdigit(buf[0]))
-        {
-            item_cnt = sscanf(buf,"%x %x %x %x",&new_addr,&v1, &v2, &v3);
-            item_cnt--;
-            if(new_addr!=addr)
-            {
-                fprintf(stderr,"warning: lost sync at address %04x (file says %04x)\n",
-                        addr, new_addr);
-            }
-        }
-        else item_cnt = sscanf(buf,"%x %x %x",&v1, &v2, &v3);
-
-        switch(item_cnt)
-        {
-            case 1:
-                data.push_back(v1);
-		addr += 1;
-                break;
-            case 2:
-                data.push_back(v1);
-                data.push_back(v2);
-		addr += 2;
-                break;
-            case 3:
-                data.push_back(v1);
-                data.push_back(v2);
-                data.push_back(v3);
-		addr += 3;
-                break;
-            default:
-                fprintf(stderr,"eeeeewwww icky!!! you got the wrong # of bytes at addr %04x.\n",
-                        addr);
-        }
-
-    }
-    if(data.size()>=65536)
-    {
-		throw std::runtime_error("ROM image larger than addressable memory!");
-    }
-    fclose(stream);
-    return;
+// Create an image from internally stored Intel Hex
+Storage::Storage(const char *intelHex[], bool readWrite) :
+	readWrite(readWrite)
+{
+	IntelHex hex(intelHex);
+	data = hex.Data();
+	address = hex.Address();
 }
 
 #if 0
